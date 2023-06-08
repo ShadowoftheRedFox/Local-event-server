0242459d76ce0da559ee804739004e09be55b210
ShadowoftheRedFox <leon.merin@gmail.com>
2023-06-08 23:43:24 +0200

    test

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 // setting up constants
-const express = require('express');
-const http = require('http');
+const express = require("express/");
+// const http = require('http');
 const app = express();
-const { WebSocketServer } = require('ws')
-const sockserver = new WebSocketServer({ port: 443, host: "192.168.1.36" })
+const ws = require('ws');
+const sockserver = new ws.Server({ port: 443, host: "192.168.1.36" })
 
 // start the websocket server
 sockserver.on('connection', ws => {
-    console.log('New client connected!')
-    ws.send('connection established')
-    ws.on('close', () => console.log('Client has disconnected!'))
+    console.log('New client connected!');
+    ws.send('connection established');
+    ws.on('close', () => console.log('Client has disconnected!'));
     ws.on('message', data => {
         sockserver.clients.forEach(client => {
-            console.log(`distributing message: ${data}`)
-            client.send(`${data}`)
-        })
-    })
+            console.log(`distributing message: ${data}`);
+            client.send(`${data}`);
+        });
+    });
     ws.onerror = function () {
-        console.log('websocket error')
+        console.log('websocket error');
     }
-})
+});
 
 // start the http server
 /**
  * @type {http.Server}
  */
-const serv = http.Server(app);
+// const serv = http.Server(app);
 
 // login token
 const token = "";
@@ -47,7 +47,7 @@ app.use('client', express.static(__dirname + '/client'))
 app.use(express.static(__dirname + '/client'));
 
 // will run on client IP and port 2000
-serv.listen(port, hostname, () => {
-    console.clear();
-    console.log(`Running on host ${hostname} on port ${port}`);
+app.listen(port, hostname, () => {
+    // console.clear();
+    console.log(`\nRunning on host ${hostname} on port ${port}`);
 });
